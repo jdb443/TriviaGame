63c52b0a08df0a944d5df5bcf12b81c2a5bb7d08
Jonathan Behar <jdb443@stern.nyu.edu>
2019-05-22 03:14:11 -0400

    question 1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,10 @@
 // Jonathan Behar Homework 5 Trivia Game
+
+var triviaQuestions = [{
+	// Question 1
+	question: "In what year was George R. R. Martin born?",
+	answerList: ["1929", "1946", "1948", "1955"],
+    answer: 2
+
+
+}];
